d98d403365fca386517dd87083e514193c0f5d54
Ahmad Asjad <ahmadcimage@gmail.com>
2019-10-16 13:34:14 +0530

    Mediator controller

---
+++ src/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ahmadasjad\yii2PlusYii1;
+
+class Controller extends \yii\web\Controller
+{
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        Yii::app()->setController($this);
+    }
+
+    public function getModule()
+    {
+        return null;
+    }
+
+    public function recordCachingAction($context,$method,$params)
+    {
+
+    }
+
+    public function createUrl($route,$params=array(),$ampersand='&')
+    {
+        return Yii::$app->getView()->createUrl($route,$params,$ampersand);
+    }
+
+}
